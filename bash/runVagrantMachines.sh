a3cb8a5547b3c02fd08c02a2d45d25323a831974
Diego Bartus <miguelanez.diego@gmail.com>
2022-10-08 20:10:05 +0200

    Added more functionality to bash script

--- bash/runVagrantMachines.sh
+++ bash/runVagrantMachines.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
 machines=( "vagrant" "vagrant2" "vagrant3" )
-common_route="/home/dbartus/code_infr_projects"
+common_route="/home/dbartus/demoProjects"
+
+
+if [[ $# -eq 0 ]]
+then
+    echo "Porfavor, Introduzca up o down como argumento"
+    exit 0
+fi
+
 
 for i in "${machines[@]}"
 do
     #Concatenate route to vagrant machine and store in temporary var
     vg_route="${common_route}/$i"
     
+    if [[ $1 = "up" ]]
+    then
     #Start the Vagrant VM
     cd $vg_route && vagrant up
     
+    elif [[ $1 = "down" ]]
+    then
+    cd $vg_route && vagrant suspend
+    
+    else
+    echo "Ha habido un error de argumentos"
+
+    fi
 done
